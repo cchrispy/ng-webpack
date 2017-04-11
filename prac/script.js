07b9622f397415d70fa1ea068a59db90e72931e5
Chris Chiang <cchiang1992@gmail.com>
2017-04-11 15:18:47 -0700

    Create prac file

---
+++ prac/script.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', []);
+
+app.controller('title', ['$scope', ($scope) => {
+  $scope.name = 'Angular practice app';
+}])
+
+app.controller('input', ['$scope', ($scope) => {
+  
+}])
